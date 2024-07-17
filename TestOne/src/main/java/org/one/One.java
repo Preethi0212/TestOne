3f0d55e2085ca568f0f618f733b064dab4f5c656
Preethi <preethisn2000@gmail.com>
2024-07-17 11:35:51 +0530

    added class One.java

---
+++ TestOne/src/main/java/org/one/One.java
@@ -0,0 +1,12 @@
+package org.one;
+
+public class One {
+private void methodOne() {
+	System.out.println("Amor");
+
+}
+public static void main(String[] args) {
+	One a = new One();
+	a.methodOne();
+}
+}
